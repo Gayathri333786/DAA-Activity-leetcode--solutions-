4591fea3441daffbc2ce10ff8826bd49f72437fd
Gayathri M <gayathri3333u@gmail.com>
2026-04-07 20:12:45 +0530

    Added Binary Search solution in C

---
+++ 5_binary_search(704).c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int binarySearch(int nums[], int n, int target) {
+    int left = 0, right = n - 1;
+
+    while(left <= right) {
+        int mid = left + (right - left) / 2;
+
+        if(nums[mid] == target)
+            return mid;
+        else if(nums[mid] < target)
+            left = mid + 1;
+        else
+            right = mid - 1;
+    }
+    return -1;
+}
+
+int main() {
+    int nums[] = {-1,0,3,5,9,12};
+    printf("%d", binarySearch(nums, 6, 9));
+}
