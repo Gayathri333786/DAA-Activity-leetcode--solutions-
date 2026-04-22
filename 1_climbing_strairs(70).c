91dac90c32ba134983f07451726bb115c738fb0d
Gayathri M <gayathri3333u@gmail.com>
2026-04-22 12:34:27 +0530

    Added climbing stairs solution in C

---
+++ 1_climbing_strairs(70).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int climbStairs(int n) {
+    if (n <= 2) return n;
+
+    int prev1 = 2, prev2 = 1, current;
+    for (int i = 3; i <= n; i++) {
+        current = prev1 + prev2;
+        prev2 = prev1;
+        prev1 = current;
+    }
+    return prev1;
+}
+
+int main() {
+    printf("%d", climbStairs(5)); 
+}
